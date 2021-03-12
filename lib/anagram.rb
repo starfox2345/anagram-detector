f7a5975cac2aaf1ce169a360c58217fab0f64384
yatsun <yatsunlam25@gmail.com>
2021-03-12 18:55:19 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    temp = []
+    def match(word)
+        word.each do |i|
+            if %w(i) == @word
+                temp << %w(i)
+   #         if %w(word) == %w(@word)
+   #            %w(word)
+            else
+            end
+        end
+    end
+    temp
+# binding.pry
+end
+
+
